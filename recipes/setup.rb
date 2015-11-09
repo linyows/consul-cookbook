e1313394eca2d43ff0cccc5bdbe8709428b1e233
linyows <linyows@gmail.com>
2015-11-09 17:57:48 +0900

    typo

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -35,7 +35,7 @@ cookbook_file '/usr/lib/systemd/system/consul.service' do
   owner 'root'
   group 'root'
   mode '0755'
-  coobook node['consul']['systemd_cookbook']
+  cookbook node['consul']['systemd_cookbook']
   notifies :restart, 'service[consul]', :delayed
 end
 
