49278de6176fe5980d8d293ef064f4986a9fd6bd
linyows <linyows@gmail.com>
2015-11-10 10:08:23 +0900

    clean up

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,28 +1,28 @@
 # Cookbook Name: consul
 # Attribute: default
 
-default['consul']['lib_path']                    = '/usr/lib/consul'
+default['consul']['lib_path']                     = '/usr/lib/consul'
 
-default['consul']['version']                     = '0.5.2'
-default['consul']['command_options']             = ['-data-dir /tmp/consul']
-default['consul']['config_cookbook']             = 'consul'
-default['consul']['systemd_cookbook']            = 'consul'
-default['consul']['download_url']                = "https://dl.bintray.com/mitchellh/consul/#{default['consul']['version']}_linux_amd64.zip"
+default['consul']['version']                      = '0.5.2'
+default['consul']['command_options']              = ['-data-dir /tmp/consul']
+default['consul']['conf_cookbook']                = 'consul'
+default['consul']['systemd_cookbook']             = 'consul'
+default['consul']['download_url']                 = "https://dl.bintray.com/mitchellh/consul/#{default['consul']['version']}_linux_amd64.zip"
 
-default['consul']['dc']                          = 'dc1'
-default['consul']['servers']                     = %w()
-default['consul']['start_join']                  = false
-default['consul']['domain']                      = 'consul'
-default['consul']['log_level']                   = 'INFO'
-default['consul']['bind_addr']                   = '0.0.0.0'
-default['consul']['client_addr']                 = '127.0.0.1'
+default['consul']['dc']                           = 'dc1'
+default['consul']['servers']                      = %w()
+default['consul']['start_join']                   = false
+default['consul']['domain']                       = 'consul'
+default['consul']['log_level']                    = 'INFO'
+default['consul']['bind_addr']                    = '0.0.0.0'
+default['consul']['client_addr']                  = '127.0.0.1'
 
-default['consul']['web_ui']['version']           = '0.5.2'
-default['consul']['web_ui']['use']               = false
-default['consul']['web_ui']['download_url']      = "https://dl.bintray.com/mitchellh/consul/#{default['consul']['version']}_web_ui.zip"
+default['consul']['web_ui']['version']            = '0.5.2'
+default['consul']['web_ui']['use']                = false
+default['consul']['web_ui']['download_url']       = "https://dl.bintray.com/mitchellh/consul/#{default['consul']['version']}_web_ui.zip"
 
-default['consul']['template']['version']         = '0.11.0'
-default['consul']['template']['download_url']    = "https://github.com/hashicorp/consul-template/releases/download/v#{default['consul']['template']['version']}/consul_template_#{default['consul']['template']['version']}_linux_amd64.zip"
-default['consul']['template']['command_options'] = []
-default['consul']['template']['config_cookbook'] = 'consul'
+default['consul']['template']['version']          = '0.11.0'
+default['consul']['template']['download_url']     = "https://github.com/hashicorp/consul-template/releases/download/v#{default['consul']['template']['version']}/consul_template_#{default['consul']['template']['version']}_linux_amd64.zip"
+default['consul']['template']['command_options']  = []
+default['consul']['template']['conf_cookbook']    = 'consul'
 default['consul']['template']['systemd_cookbook'] = 'consul'
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'linyows@gmail.com'
 license          'MIT'
 description      'Installs/Configures consul'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.3'
+version          '0.2.4'
 
 recipe 'consul', 'Installs and configures consul'
 recipe 'consul::template', 'Installs and configures consul-template'
