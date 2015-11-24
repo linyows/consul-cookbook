65e19077184d0a57e15fba4308a9ac6f409d54f7
linyows <linyows@gmail.com>
2015-11-24 12:10:24 +0900

    use go-retry instead of retry.sh

--- recipes/retry.rb
+++ recipes/retry.rb
@@ -1,10 +1,21 @@
 # Cookbook Name:: consul
 # Recipe:: retry
 
-cookbook_file "#{node['consul']['lib_path']}/bin/retry" do
-  source 'retry.sh'
-  user 'consul'
-  group 'consul'
-  mode '0755'
-  notifies :restart, 'service[consul]', :delayed
+cache_path = Chef::Config[:file_cache_path]
+bin_path = '/usr/bin'
+
+remote_file "#{cache_path}/retry-#{node['consul']['retry']['version']}.zip" do
+  source node['consul']['retry']['download_url']
+  action :create_if_missing
+end
+
+bash "expand retry-#{node['consul']['retry']['version']}" do
+  not_if "which retry && test $(retry -version 2>&1 | awk '{print $3}') = '#{node['consul']['retry']['version']}'"
+  code <<-CODE
+    cd "#{cache_path}"
+    unzip retry-#{node['consul']['retry']['version']}.zip
+    chmod +x retry
+    rm -rf #{bin_path}/retry
+    mv retry #{bin_path}/retry
+  CODE
 end
