43525ee397453bc5c5ffefedd14d2c21367fac8b
linyows <linyows@gmail.com>
2017-03-23 23:57:24 +0900

    bump to default consul version

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,7 @@
 
 default['consul']['lib_path']                     = '/usr/lib/consul'
 
-default['consul']['version']                      = '0.7.2'
+default['consul']['version']                      = '0.7.5'
 default['consul']['data_dir']                     = '/tmp/consul'
 default['consul']['command_options']              = ["-data-dir #{default['consul']['data_dir']}"]
 default['consul']['conf_cookbook']                = 'consul'
