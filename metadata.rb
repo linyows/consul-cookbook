f649001fd78dffd1b80e2e03c4083a72847e1848
linyows <linyows@gmail.com>
2017-07-01 22:45:03 +0900

    bump version to 0.13.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'linyows@gmail.com'
 license          'MIT'
 description      'Installs/Configures consul'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.12.1'
+version          '0.13.0'
 
 recipe 'consul', 'Installs and configures consul'
 recipe 'consul::template', 'Installs and configures consul-template'
