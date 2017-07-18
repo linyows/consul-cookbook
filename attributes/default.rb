fcbccb5ffd2488113e6df7ad3cb1ad637fc83809
linyows <linyows@gmail.com>
2017-07-18 12:44:28 +0900

    use consul webui version as url

--- attributes/default.rb
+++ attributes/default.rb
@@ -24,7 +24,7 @@ default['consul']['multiple_private_ips']         = false
 
 default['consul']['web_ui']['version']            = '0.9.0-rc1'
 default['consul']['web_ui']['use']                = false
-default['consul']['web_ui']['download_url']       = "https://releases.hashicorp.com/consul/#{default['consul']['version']}/consul_#{default['consul']['version']}_web_ui.zip"
+default['consul']['web_ui']['download_url']       = "https://releases.hashicorp.com/consul/#{default['consul']['web_ui']['version']}/consul_#{default['consul']['web_ui']['version']}_web_ui.zip"
 
 default['consul']['template']['version']          = '0.18.5'
 default['consul']['template']['download_url']     = "https://releases.hashicorp.com/consul-template/#{default['consul']['template']['version']}/consul-template_#{default['consul']['template']['version']}_linux_amd64.zip"
